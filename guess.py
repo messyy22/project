0d8bac8bacfbadd82b074f204b72960f423a53d7
messyy22 <rahulchamyal2013@gmail.com>
2025-02-19 14:48:13 +0530

    guess.py

--- guess.py
+++ guess.py
@@ -1,17 +1,39 @@
 import random
-
-def guess_the_number():
-    number_to_guess = random.randint(1, 100)
-    attempts = 0
-    while True:
-        guess = int(input("Guess the number (between 1 and 100): "))
-        attempts += 1
-        if guess < number_to_guess:
+def game():
+    print("Welcome to the number guessing game!")
+    with open("highscore.txt","r") as f:
+        highscore=f.read()
+        if highscore=="":
+            highscore=int (highscore)
+            print(f"The highscore is {highscore}")
+        else:
+            print(f"The highscore is {highscore}")
+    print("I'm thinking of a number between 1 and 100")
+    number=random.randint(1,100)
+    guess=0
+    score=0
+    while(guess!=number):
+        score+=1
+        guess=int(input("Enter your guess:"))
+        if guess<number:
             print("Too low!")
-        elif guess > number_to_guess:
+        elif guess>number:
             print("Too high!")
         else:
-            print(f"Correct! You guessed it in {attempts} attempts.")
+            print("You got it!")
             break
+    print(f"Number of guesses you took :{score}")
+    if(score > highscore):
+        print("Congratulations! You have broken the highscore!")
+        with open("highscore.txt","w") as f:
+            f.write(str(score))
+    else:
+        print("You could not break the highscore. Better luck next time!")
+    play_again=input("Do you want to play again? (yes/no):")
+    if play_again=="yes":
+        game()
+    else:
+        print("Thanks for playing!")
 
-guess_the_number()
+
+game()
